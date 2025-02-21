c80328e3e1fff34e91f534fe410db1db48126187
Baptngr <baptiste.nogueras@mmibordeaux.com>
2025-02-21 12:20:58 +0100

    TEAM

--- db/migrate/20250220135754_create_matches.rb
+++ db/migrate/20250220135754_create_matches.rb
@@ -5,14 +5,16 @@ class CreateMatches < ActiveRecord::Migration[8.0]
       t.integer :score_away
       t.text :description
       t.date :date
+      
       t.references :team_home, null: false, foreign_key: { to_table: :teams }
       t.references :team_away, null: false, foreign_key: { to_table: :teams }
       t.references :day, null: false, foreign_key: true
 
-      t.references :team_home, null: false, foreign_key: { to_table: :teams }
-      t.references :team_away, null: false, foreign_key: { to_table: :teams }
-      t.references :day, null: false, foreign_key: true
+
+  
       t.timestamps
+
+      
     end
   end
 end
