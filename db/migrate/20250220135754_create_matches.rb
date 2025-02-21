3e9ae1a5ad99eabec4edbac40bf06d8706f4967a
Yanis-310 <taombeyanis@gmail.com>
2025-02-21 11:18:32 +0100

    db create

--- db/migrate/20250220135754_create_matches.rb
+++ db/migrate/20250220135754_create_matches.rb
@@ -1,18 +1,14 @@
-class CreateJoinTableDaysTeamsMatches < ActiveRecord::Migration[8.0]
+class CreateMatches < ActiveRecord::Migration[8.0]
   def change
-    create_table :matches_days_teams do |t|
+    create_table :matches do |t|
       t.integer :score_home
       t.integer :score_away
       t.text :description
       t.date :date
-      t.belongs_to :day
-      t.belongs_to :team
-
 
       t.references :team_home, null: false, foreign_key: { to_table: :teams }
       t.references :team_away, null: false, foreign_key: { to_table: :teams }
       t.references :day, null: false, foreign_key: true
-
       t.timestamps
     end
   end
